305c54186db0b0c1c1df1b706cdd03c5c9073a69
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-30 10:31:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam"
+  };
+  
+ 
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee }; 
+    delete newEmployee[key]; 
+    return newEmployee; 
+  }
+  
+ 
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+  }
+  
+ 
+  console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway")); 
+
+  
+  console.log(employee); 
+ 
+  
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+  console.log(employee); 
+  
+  
+  console.log(deleteFromEmployeeByKey(employee, "name")); 
+ 
+  
+  console.log(employee); 
+  
+  
+  destructivelyDeleteFromEmployeeByKey(employee, "name");
+  console.log(employee); 
+  
